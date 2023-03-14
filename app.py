c18ac3fedd361590996862e1d0d7ee711db2ad15
Paulo Talarico <66432564+PauloTalarico287@users.noreply.github.com>
2023-03-13 22:09:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+menu = """
+<a href="/">Página Inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a>
+<br>
+"""
+
 @app.route("/")
 def hello_world():
-  return "Olá, mundo! Esse é meu site. (Paulo Talarico)"
+  return menu + "Olá, mundo! Esse é meu site. (Paulo Talarico)"
 
 @app.route("/sobre")
 def sobre():
-  return "Aqui vai o conteúdo da página Sobre"
+  return menu + "Aqui vai o conteúdo da página Sobre"
 
 @app.route("/contato")
 def contato():
-  return "Aqui vai o conteúdo da página Contato"
+  return menu + "Aqui vai o conteúdo da página Contato"
