894aa230d34685272bd4f2add6962549ccf1e82f
Paulo Talarico <66432564+PauloTalarico287@users.noreply.github.com>
2023-03-20 20:50:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -26,12 +26,6 @@ def promocoes():
   <br>
   <ul>
   """
-  scraper = ChannelScraper()
-  contador = 0
-  for message in scraper.messages("promocoeseachadinhos"):
-    contador += 1
-    texto = message.text.strip().splitlines()[0]
-    conteudo += f"<li>{message.created_at} {texto}</li>"
-    if contador == 10:
-      break
+  for promocao in ultimas_promocoes():
+    conteudo += f"<li>{promocao}</li>"
   return conteudo + "</ul>"
