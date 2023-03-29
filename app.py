ab0b3a0609e79f39acfdbbcaa629297878ea0a92
Paulo Talarico <66432564+PauloTalarico287@users.noreply.github.com>
2023-03-29 00:37:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -152,29 +152,4 @@ def telegram_bot():
     texto_resposta = "Não entendi! Para outras informações sobre a cidade de São Paulo, acesse o site da Agência Mural: agenciamural.org.br"
   nova_mensagem = {"chat_id": chat_id, "text": texto_resposta}
   requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-  
-  resposta = requests.get(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/getUpdates")
-  dados = resposta.json()["result"]  
-  for update in dados:
-    mensagens = []
-    update_id = update["update_id"]
-    if update_id in updates_processados:
-      continue
-      first_name = update["message"]["from"]["first_name"]
-      sender_id = update["message"]["from"]["id"]
-      if "text" not in update["message"]:
-        continue
-      message = update["message"]["text"]
-      chat_id = update["message"]["chat"]["id"]
-      datahora = str(datetime.datetime.fromtimestamp(update["message"]["date"]))
-      if "username" in update["message"]["from"]:
-        username = f' @{update["message"]["from"]["username"]}'
-      else:
-        username = "Não definido"
-      mensagens.append([datahora, "recebida", username, first_name, chat_id, message])
-      mensagens.append([datahora, "enviada", username, first_name, chat_id, texto_resposta])
-      updates_processados.append(update_id)
-      sheet.update("A1", update_id)
-      sheet2.append_row([datahora, first_name, username, sender_id, message])  
-      sheet3.append_rows(mensagens)
-      return "ok"
+  return "ok"
