29d90a9b63f86c559fba100dcf4e4ddc1c7cdcf5
Paulo Talarico <66432564+PauloTalarico287@users.noreply.github.com>
2023-04-04 08:50:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -215,19 +215,18 @@ def coleta():
     cidades=[osasco, guarulhos, sao_bernardo, carapicuiba, taboao_da_serra, cotia, itaquaquecetuba, suzano, diadema, barueri]
 
     for cidade in cidades:
-      requisicao=requests.get(cidade)
-      html=BeautifulSoup(requisicao.content)
-      leis = html.find_all('li',{'class':'item item-result index-leismunicipais'})
-      Cidade=html.find('title').text
-      leis_cidades=[]
-      for law in leis:
-        Título = law.find('h3',{'class':'title'}).text.replace("Norma em vigor", "").strip()
-        Descrição = law.find('p',{'class':'description'}).text.strip()
-        Link = f"https://leismunicipais.com.br{law.find('a').get('href')}"
-        leis_cidades.append([Cidade, Título, Descrição, Link])
+    requisicao = requests.get(cidade)
+    html = BeautifulSoup(requisicao.content, "html.parser")
+    leis = html.find_all('li', {'class':'item item-result index-leismunicipais'})
+    cidade_nome = html.find('title').text
+    for law in leis:
+      titulo = law.find('h3', {'class':'title'}).text.replace("Norma em vigor", "").strip()
+      descricao = law.find('p', {'class':'description'}).text.strip()
+      link = f"https://leismunicipais.com.br{law.find('a').get('href')}"
+      leis_cidades.append([cidade_nome, titulo, descricao, link])
 
-    df = pd.DataFrame(leis_cidades, columns=['Cidade','Título', 'Descrição', 'Link'])
-
+    df = pd.DataFrame(leis_cidades, columns=['Cidade', 'Título', 'Descrição', 'Link'])
+  
     valores = sheet_leis.col_values(4)
     novos_links = [link for link in df['Link'] if link not in valores]
 
