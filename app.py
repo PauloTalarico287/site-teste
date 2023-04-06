9bf114a068257bdfe4d8eef17171e6cf050fbc6d
Paulo Talarico <66432564+PauloTalarico287@users.noreply.github.com>
2023-04-06 13:27:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -37,12 +37,6 @@ menu = """
 def hello_world():
   return menu + "Olá, mundo! Esse é meu site. (Paulo Talarico)"
 
-@app.route("/dedoduro")
-def dedoduro():
-  mensagem = {"chat_id": TELEGRAM_ADMIN_ID, "text": "Alguém acessou a página dedo duro!"}
-  requests.post(f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/sendMessage", data=mensagem)
-  return "Mensagem enviada."
-
 @app.route("/sobre")
 def sobre():
   return menu + "Aqui vai o conteúdo da página Sobre"
@@ -51,37 +45,6 @@ def sobre():
 def contato():
   return menu + "Aqui vai o conteúdo da página Contato"
 
-@app.route("/promocoes")
-def promocoes():
-  conteudo = menu + """
-  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
-  <br>
-  <ul>
-  """
-  scraper = ChannelScraper()
-  contador = 0
-  for message in scraper.messages("promocoeseachadinhos"):
-    contador += 1
-    texto = message.text.strip().splitlines()[0]
-    conteudo += f"<li>{message.created_at} {texto}</li>"
-    if contador == 10:
-      break
-  return conteudo + "</ul>"
-
-@app.route("/dedoduro2")
-def dedoduro2():
-  sheet.append_row(["Paulo", "Talarico", "a partir do Flasck"])
-  return "Planilha escrita!"
-
-#@app.route("/telegram-bot", methods=["POST"])
-#def telegram_bot():
-  #update = request.json
-  #chat_id = update["message"]["chat"]["id"]
-  #message = update["message"]["text"]
-  #nova_mensagem = {"chat_id": chat_id, "text": message}
-  #requests.post(f"https://api.telegram.org./bot{TELEGRAM_API_KEY}/sendMessage", data=nova_mensagem)
-
-  
 @app.route("/telegram-bot", methods=['POST'])
 def telegram_bot():
   update = request.json
