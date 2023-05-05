ad225e2f2e16375cfa67b901d71be6bc4559ad52
Paulo Talarico <66432564+PauloTalarico287@users.noreply.github.com>
2023-05-04 21:24:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -259,9 +259,10 @@ def coleta():
       sheet_leis.append_rows(novos_dados.values.tolist())
       novos_dados['Link'] = novos_dados['Link'].apply(lambda x: f'<a href="{x}">{x}</a>')
       lista_emails=['paulotbastos@hotmail.com', 'paulo@agenciamural.org.br']
+      to_emails = ",".join(lista_emails)
       message = Mail(
         from_email='paulo@agenciamural.org.br',
-        to_emails= lista_emails
+        to_emails=to_emails,
         subject='Confira as últimas leis sancionadas',
         html_content=f'Olá! Seguem as últimas leis e decretos publicados em cidades da Grande SP: {novos_dados.to_html(escape=False)}'
         )  
