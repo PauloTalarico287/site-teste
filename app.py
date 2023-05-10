deae96c7ee061d5e5559c484cded3a6c0033ff49
Paulo Talarico <66432564+PauloTalarico287@users.noreply.github.com>
2023-05-10 11:49:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -265,7 +265,7 @@ def coleta():
           to(email),
           subject='Confira as últimas leis sancionadas',
           html_content=f'Olá! Seguem as últimas leis e decretos publicados em cidades da Grande SP: {novos_dados.to_html(escape=False)}'
-          )
+        )
         sg = SendGridAPIClient(SENDGRID_KEY)
         response = sg.send(message)
      
